9dfc0ca2fe78af7ca5768930be083817d8b97be7
Moritz Hesche <mo.hesche@gmail.com>
2023-02-03 08:29:35 +0100

    datatypes

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/datatypes/datatypes.py
@@ -0,0 +1,37 @@
+"""This module contains only dataclasses. These dataclasses hold only information and no
+logic. They are used to define the datatypes used in the project.
+The datatypes can be grouped into two categories:
+- DatasetSamples: These are the samples that are returned by the datasets
+There should be an compatible preprocessing pipeline which accepts these samples as input
+in order to used them in the project.
+- Results: These are the results that are returned by the posprocessing steps
+There should be an compatible postprocessing pipeline which outputs these dataclasses as results.
+"""
+from dataclasses import dataclass
+from abc import ABC
+
+
+@dataclass
+class DatasetSample(ABC):
+    pass
+
+
+@dataclass
+class {{cookiecutter.dataset_name}}Sample(DatasetSample):
+    attr1: ...
+    attr2: ...
+    attr3: ...
+
+# ... dataclasses for other datasets here
+
+@dataclass
+class ResultBase(ABC):
+    pass
+
+@datclass
+class SomeResult(ResultBase):
+    attr1: ...
+    attr2: ...
+    attr3: ...
+
+# ... dataclasses for other results here
