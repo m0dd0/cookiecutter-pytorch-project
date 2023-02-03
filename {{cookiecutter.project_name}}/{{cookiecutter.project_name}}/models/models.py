64bd63259685c549e2fb512b815d4da4291efd40
Moritz Hesche <mo.hesche@gmail.com>
2023-02-03 08:29:48 +0100

    models

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/models/models.py
@@ -0,0 +1,46 @@
+""" This module contains the network architecture(s) of the project."""
+
+import torch.nn as nn
+
+from torchtyping import TensorType
+
+from .custom_modules import CustomModuleExample
+
+
+class {{cookiecutter.network_name}}(nn.Module):
+    @classmethod
+    def from_state_dict_path(
+        cls, model_path: Path = None, device: str = None
+    ) -> "{{cookiecutter.network_name}}":
+        if device is None:
+            device = (
+                torch.device("cuda")
+                if torch.cuda.is_available()
+                else torch.device("cpu")
+            )
+
+        if model_path is None:
+            model_path = (
+                Path(__file__).parent.parent
+                / "checkpoints"
+                / "default_weights.pt"
+            )
+
+        model = cls()
+        model.load_state_dict(torch.jit.load(model_path).state_dict())
+        model.to(device)
+
+        return model
+
+    def __init__(self):
+        super().__init__()
+
+        # TODO: configure the modules of the network here
+
+        raise NotImplementedError()
+
+    def forward(self, x: TensorType) -> TensorType:
+        # TODO: implement the forward pass of the network here
+        
+        raise NotImplementedError()
+    
