9eb0085c417caa0f8ad27c4f685c06cc8d89c6d8
Moritz Hesche <mo.hesche@gmail.com>
2023-02-13 18:58:28 +0100

    model params example

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/models/models.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/models/models.py
@@ -10,23 +10,29 @@ from .custom_modules import CustomModuleExample
 class {{cookiecutter.model_name}}(nn.Module):
     @classmethod
     def from_state_dict_path(
-        cls, model_path: Path = None, device: str = None
+        cls, model_path: Path = None, device: str = None, *args, **kwargs
     ) -> "{{cookiecutter.model_name}}":
         device = device or (
             torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         )
         model_path = model_path or Path(__file__).parent.parent / "checkpoints" / "default_weights.pt"
 
-        model = cls()
+        model = cls(*args, **kwargs)
         model.load_state_dict(torch.jit.load(model_path).state_dict())
         model.to(device)
 
         return model
 
-    def __init__(self):
+    def __init__(
+            self, 
+            # model_param_1, 
+            # model_param_2
+        ):
         super().__init__()
 
         # TODO: configure the modules of the network here
+        # self.model_param_1 = model_param_1
+        # self.model_param_2 = model_param_2
 
         raise NotImplementedError()
 
